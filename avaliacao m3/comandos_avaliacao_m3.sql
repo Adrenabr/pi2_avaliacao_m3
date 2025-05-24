295bd0a9fb82b07fe12405dd7e4dd388a8f6abe5
Adrenabr <keltonrudenas@live.com>
2025-05-24 08:59:03 -0400

    Criar tabelas usuarios e clientes

---
+++ avaliacao m3/comandos_avaliacao_m3.sql
@@ -0,0 +1,43 @@
+CREATE TABLE usuarios (
+    usuario_id SERIAL PRIMARY KEY,
+    nome_usuario VARCHAR(50) UNIQUE NOT NULL,
+    senha_hash VARCHAR(255) NOT NULL, -- verificar bcrypt ou Argon2
+    email_usuario VARCHAR(100) UNIQUE NOT NULL, -- login ou recuperacao, implementar
+    token_recuperacao_senha VARCHAR(255) UNIQUE, -- token de recuperacao(com data/hora de expiração), conferir necessidade de tamanho
+    data_ultima_alteracao_senha TIMESTAMP WITHOUT TIME ZONE,
+    tentativas_login_falhas INT DEFAULT 0, -- contator de tentativas falhas para bloquear login
+    data_bloqueio TIMESTAMP WITHOUT TIME ZONE,
+    codigo_verificacao VARCHAR(6), -- código para verificacao de email ou telefone
+    data_expiracao_codigo_verificacao TIMESTAMP WITHOUT TIME ZONE, -- data e hora para expirar codigo de verificacao
+    data_cadastro TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+    ultimo_login TIMESTAMP WITHOUT TIME ZONE,
+    ativo BOOLEAN DEFAULT TRUE NOT NULL, -- se necessario ativação via email mudar
+    cliente_id INT REFERENCES clientes(cliente_id), -- FK para vincular a tabela clientes
+    role_usuario VARCHAR(50) DEFAULT 'cliente', -- verificar necessidade de separar da tabela usuarios
+    --tipo_usuario INT NOT NULL, -- verificar antes de implementar
+    primeiro_nome VARCHAR(50),
+    ultimo_nome VARCHAR(50),
+    foto_perfil VARCHAR(255) -- caminho para img de perfil do usuario
+);
+CREATE TABLE clientes (
+    cliente_id SERIAL PRIMARY KEY,
+    nome_cliente VARCHAR(100) NOT NULL, -- caso PJ, o responsável
+    email VARCHAR(100) UNIQUE,
+    tipo_cliente VARCHAR(2) CHECK (tipo_cliente IN ('PF', 'PJ')),
+    cpf VARCHAR(14) UNIQUE,
+    data_nascimento DATE, -- apenas para PF, implementar lógica de negócio
+    cnpj VARCHAR(18) UNIQUE,
+    inscricao_estadual VARCHAR(20), -- apenas para PJ, implementar lógica de negócio
+    razao_social VARCHAR(100), -- apenas para PJ, implementar lógica de negócio
+    nome_fantasia VARCHAR(100),  -- apenas para PJ, implementar lógica de negócio
+    telefone VARCHAR(20),
+    cep VARCHAR(10),
+    endereco VARCHAR(255),
+    complemento_endereco VARCHAR(50),
+    bairro VARCHAR(50),
+    cidade VARCHAR(50),
+    estado VARCHAR(2),
+    observacoes TEXT,
+    status_cliente VARCHAR(20),
+    data_cadastro TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
+);
